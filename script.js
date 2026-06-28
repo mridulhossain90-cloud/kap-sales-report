161f88e0b68a88e153015da8554c3e9decac14a5
mridulhossain90-cloud <mridulhossain90@gmail.com>
2026-06-28 15:56:17 +0600

    Initial KAP project setup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("KAP Sales Reporting System started");
+const loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "" || password === "") {
+    alert("Please enter email and password");
+  } else {
+    alert("Login button is working");
+  }
+});
